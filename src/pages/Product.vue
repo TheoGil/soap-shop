33097a5ebee121c9be85523838a9c2fd60052969
= <=>
2017-12-28 13:32:41 +0100

    Product page: actions

--- src/pages/Product.vue
+++ src/pages/Product.vue
@@ -28,14 +28,13 @@
     </div>
 
     <!-- Product Header -->
-    <div class="row">
+    <div class="row flex">
       <div class="columns small-8">
         <h1 class="product-title" itemprop="name">Bulles d'amande douce</h1>
         <div class="notation">
           <stars :note="4"/>
           <div class="review-counter">(32)</div>
         </div>
-        
       </div>
       <div class="columns small-4">
         <div class="product-price">
@@ -46,16 +45,44 @@
     </div>
     <!-- /Product Header -->
 
-    <!-- Product Actions -->
-    <div class="row column">
-      <div class="product-actions left">
-        tada
+    <!-- Labels and actions  -->
+    <div class="row column product-labels-actions-container">
+      <div class="product-labels">
+        <div class="badge">
+          <svg class="icon">
+            <use xlink:href="#icon-binoculars"></use>
+          </svg>
+        </div>
+        <div class="badge">
+          <svg class="icon">
+            <use xlink:href="#icon-leaf"></use>
+          </svg>
+        </div>
+        <div class="badge">
+          <svg class="icon">
+            <use xlink:href="#icon-bookmark"></use>
+          </svg>
+        </div>
       </div>
-      <div class="product-actions right">
-        dada
+      <div class="product-actions">
+        <a class="badge product-buy" href="#">
+          <svg class="icon">
+            <use xlink:href="#icon-cart"></use>
+          </svg>
+        </a>
+        <a class="badge" href="#">
+          <svg class="icon">
+            <use xlink:href="#icon-heart"></use>
+          </svg>
+        </a>
+        <a class="badge share" href="#">
+          <svg class="icon">
+            <use xlink:href="#icon-share"></use>
+          </svg>
+        </a>
       </div>
     </div>
-    <!-- /Product Actions -->
+    <!-- /Labels and actions -->
 
     <!-- Product Slideshow -->
     <div class="row column">
