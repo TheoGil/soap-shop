7221d9165404c3101260736a599d4207fe488c83
= <=>
2018-01-27 12:34:20 +0100

    corrections listing produit

--- src/components/ProductThumbnail.vue
+++ src/components/ProductThumbnail.vue
@@ -1,39 +1,37 @@
 <template>
-  <transition appear name="fade">
-    <div class="product-wrapper-outer">
-      <div class="product-wrapper-inner">
-        <div class="product">
-          <router-link to="/product" class="navbar-entry">
-            <div class="product-thumbnail-placeholder">
-              <svg class="icon" width="200" height="200">
-                  <use xlink:href="#icon-logo"></use>
-                </svg>
-            </div>
-            <img 
-              v-lazy="thumbnail"
-              alt="" 
-              class="product-thumbnail lazy"
-            >
-            <div class="product-infos">
-              <div class="product-name product-data">{{ title }}</div>
-              <div class="product-name product-data">{{ price }}</div>
-              <div class="product-name product-data product-icons">
-                <svg class="icon">
-                  <use xlink:href="#icon-binoculars"></use>
-                </svg>
-                <svg class="icon">
-                  <use xlink:href="#icon-leaf"></use>
-                </svg>
-                <svg class="icon">
-                  <use xlink:href="#icon-bookmark"></use>
-                </svg>
-              </div>
-            </div>
-          </router-link>
+  <div class="product">
+    <router-link to="/product">
+      <div class="product-thumbnail-placeholder">
+        <svg class="icon" width="200" height="200">
+          <use xlink:href="#icon-logo"></use>
+        </svg>
+      </div>
+      <div class="product-thumbnail-wrapper-outer">
+        <div class="product-thumbnail-wrapper-inner">
+          <img
+            v-lazy="thumbnail"
+            alt=""
+            class="product-thumbnail lazy"
+          >
         </div>
       </div>
-    </div>
-  </transition>
+      <div class="product-infos">
+        <div class="product-name product-data bold">{{ title }}</div>
+        <div class="product-price product-data">{{ price }}</div>
+        <div class="product-name product-data product-icons">
+          <svg class="icon">
+            <use xlink:href="#icon-binoculars"></use>
+          </svg>
+          <svg class="icon">
+            <use xlink:href="#icon-leaf"></use>
+          </svg>
+          <svg class="icon">
+            <use xlink:href="#icon-bookmark"></use>
+          </svg>
+        </div>
+      </div>
+    </router-link>
+  </div>
 </template>
 <script>
   export default {
